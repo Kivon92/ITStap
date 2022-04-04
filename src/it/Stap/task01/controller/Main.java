344205e1b0e5e7c7dc3da61c6079b6ba85984d4d
Kivon92 <102616301+Kivon92@users.noreply.github.com>
2022-04-04 20:14:14 +0300

    Add

---
+++ src/it/Stap/task01/controller/Main.java
@@ -0,0 +1,17 @@
+package it.Stap.task01.controller;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number");
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int y2 = sc.nextInt();
+        boolean res = true;
+        String  msg = res ? "yes" : "no";
+        System.out.println(msg);
+    }
+}
---
+++ src/it/Stap/task01/logic/Chess.java
@@ -0,0 +1,7 @@
+package it.Stap.task01.logic;
+
+public class Chess {
+    public static boolean checkRookStep(int x1,int y1,int x2,int y2){
+        return false;
+    }
+}
