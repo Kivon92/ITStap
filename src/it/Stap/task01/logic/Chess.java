a736ed1b3236b2e10ae4b49d2541a7190b5831f1
Kivon92 <102616301+Kivon92@users.noreply.github.com>
2022-04-04 21:54:56 +0300

    Add Chhess

--- src/it/Stap/task01/logic/Chess.java
+++ src/it/Stap/task01/logic/Chess.java
@@ -2,6 +2,8 @@ package it.Stap.task01.logic;
 
 public class Chess {
     public static boolean checkRookStep(int x1,int y1,int x2,int y2){
-        return false;
+
+        return(x1 == x2 && y1 !=y2 || x1 != x2 && y1 ==y2 );
     }
+
 }
